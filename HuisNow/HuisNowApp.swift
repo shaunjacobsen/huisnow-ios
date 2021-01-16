64da282e2e064dbd19f15f68fb0db9e313a76e46
Shaun Jacobsen <shaun@sjacobsen.com>
2021-01-16 21:09:37 +0100

    Initial Commit

---
+++ HuisNow/HuisNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuisNowApp.swift
+//  HuisNow
+//
+//  Created by Shaun Jacobsen on 16/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct HuisNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
