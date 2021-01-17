b60a52a11fab23cc2258fd355db9bae260b88f0a
Shaun Jacobsen <shaun@sjacobsen.com>
2021-01-17 16:45:48 +0100

    Creates PropertyRowView

--- HuisNow/Views/PropertyListView.swift
+++ HuisNow/Views/PropertyListView.swift
@@ -7,14 +7,88 @@
 
 import SwiftUI
 
+
+
 struct PropertyListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @Binding var properties: [Property]
+  var body: some View {
+    ZStack {
+      Color("Background")
+      ForEach(properties.indices) { i in
+        PropertyListRow(property: properties[i])
+      }
     }
+    
+    
+  }
 }
 
-struct Property_Previews: PreviewProvider {
-    static var previews: some View {
-      PropertyListView()
+struct PropertyListRow: View {
+  let property: Property
+  
+  var body: some View {
+    ZStack {
+      Color(.white)
+      HStack {
+        property.images[0]
+          .centerCropped()
+          .frame(width: 150)
+        
+        Group {
+          VStack(alignment: .leading) {
+            HStack {
+              Text(property.name)
+                .font(.title3)
+                .kerning(-1)
+              Spacer()
+              property.statusSymbol()
+            }.padding(.trailing, 10)
+            
+            
+            // address
+            VStack {
+              HStack {
+                Text(property.address)
+                  .font(.caption)
+                Spacer()
+              }
+              
+              
+              HStack {
+                Text(property.postcode)
+                  .font(.caption)
+                Text(property.municipality)
+                  .font(.caption)
+                Spacer()
+              }
+            }
+            Spacer()
+            // price
+            Text(property.formattedPrice())
+              .font(.title3)
+              .fontWeight(.medium)
+          }
+          .padding(.top, 10)
+          .padding(.bottom, 10)
+        }
+      }
+      
     }
+    
+    .cornerRadius(6.0)
+    .shadow(color: Color(.black).opacity(0.2), radius: 5, x: 0, y: 0)
+    .frame(maxWidth: .infinity, maxHeight: 150)
+    .badge()
+    .padding()
+  }
+}
+
+
+
+struct Property_Previews: PreviewProvider {
+  static private var properties = Binding.constant([Property(name: "Property One", type: .apartment, address: "Soendastraat 24", postcode: "1094 BH", price: 150000, agent: "Makelaar", images: [Image("SampleProperty")], municipality: "Amsterdam", constructionYear: 1930, source: "Pararius", sourceIdentifier: "some identifier", createdAt: Date(), updatedAt: Date(), status: .new)])
+  
+  static var previews: some View {
+    PropertyListView(properties: properties)
+  }
 }
--- Property.swift
+++ Property.swift
@@ -6,23 +6,58 @@
 //
 
 import Foundation
+import SwiftUI
 
 enum PropertyType {
   case apartment
   case townhouse
 }
 
+enum PropertyStatus {
+  case new
+  case markedInterested
+  case viewingBooked
+}
+
 struct Property {
   let name: String
-  let type: PropertyType
+  let type: PropertyType?
   let address: String
   let postcode: String
   let price: Int
   let agent: String
-  let images: [String]
+  let images: [Image]
+  let municipality: String
   let constructionYear: Int
   let source: String
   let sourceIdentifier: String
   let createdAt: Date
   let updatedAt: Date
+  let status: PropertyStatus
+}
+
+extension Property {
+  func formattedPrice() -> String {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .currency
+    formatter.maximumFractionDigits = 0
+    formatter.currencyCode = "EUR"
+    var formattedPrice: String = ""
+    if let price = formatter.string(from: self.price as NSNumber) {
+      formattedPrice = price
+    }
+    
+    return formattedPrice
+  }
+  
+  func statusSymbol() -> Image? {
+    switch self.status {
+    case .markedInterested:
+      return Image(systemName: "star.fill")
+    case .viewingBooked:
+      return Image(systemName: "calendar.circle")
+    case .new:
+      return nil
+    }
+  }
 }
